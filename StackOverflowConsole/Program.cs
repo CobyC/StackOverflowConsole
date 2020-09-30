503d8e9103c1c10e750978a6fad54ef00d094115
C0byC <coby@capricorncoding.com>
2020-09-30 18:56:10 +0100

    another answer

--- StackOverflowConsole/Program.cs
+++ StackOverflowConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace StackOverflowConsole
 {
@@ -6,7 +7,40 @@ namespace StackOverflowConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //int age;
+            //Console.Write("Please input your age: ");
+            //age = Convert.ToInt32(Console.ReadLine());
+            //Console.Write(age);
+            //Console.Write("\n");
+
+            //if (age < 18)
+            //{
+            //    Console.Write("You cannot cast your vote!");
+            //}
+            //else
+            //    Console.Write("You can cast your vote!");
+            //{
+
+            //}
+
+            try
+            {                               
+                TextReader tr = Console.In;
+                char[] buffer = new char[4];
+                tr.Read(buffer, 0, 4);
+                string consoleOutput = new string(buffer);
+                if (consoleOutput != 18.ToString())
+                {
+                    Console.WriteLine($"Expected 18, you entered {consoleOutput}");
+                }
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+
+
         }
     }
 }
